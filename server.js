1e3d618a42500450f22ec4f3605b77ff2881d9c0
subodhsawan <subodh.sawan@gmail.com>
2021-01-11 18:21:29 +0530

    'finaaal'

--- server.js
+++ server.js
@@ -8,11 +8,10 @@ const app= express();
 app.use(express.static(__dirname + '/dist/Project'));
 app.get('*' , function(req,res){
   //replace the '/dist to your priject name>index.html'
-<<<<<<< HEAD:server.js
-  res.sendFile(path.join(__dirname +'/dist//Project/index.html'));
-=======
+
+
   res.sendFile(path.join(__dirname +'/dist/Project/index.html'));
->>>>>>> 124d2ec90bc63892ac4c5cc87ccb8fe4d0d42eab:src/server.js
+
 
 });
 //start the app by listeninh on the default heruku port
