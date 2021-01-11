124d2ec90bc63892ac4c5cc87ccb8fe4d0d42eab
Subodh91008 <76906758+Subodh91008@users.noreply.github.com>
2021-01-11 12:50:35 +0530

    Update server.js
    forth chane

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app= express();
 // server only static file from tje dist directory
 // replace the '/dist/<to your project name></to>'
-app.use(express.static(__dirname + '/dist/angularfronten1d'));
+app.use(express.static(__dirname + '/dist/Project'));
 app.get('*' , function(req,res){
   //replace the '/dist to your priject name>index.html'
-  res.sendFile(path.join(__dirname +'/dist/angularfronten1d/index.html'));
+  res.sendFile(path.join(__dirname +'/dist/Project/index.html'));
 
 });
 //start the app by listeninh on the default heruku port
